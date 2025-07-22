17349a8e597a61d4fd562451a3237eef96596907
Laraib Ahmed <Laraib.ahmed@unibz.it>
2025-07-22 22:23:59 +0200

    card stuff more 3

--- server.js
+++ server.js
@@ -269,7 +269,9 @@ app.post('/capture_payment_intent', async (req, res) => {
 
 			invoice = await stripe.invoices.finalizeInvoice(invoice.id);
 			if (invoice.status !== 'paid') {
-				await stripe.invoices.pay(invoice.id);
+				await stripe.invoices.pay(invoice.id, {
+					paymentIntent: payment_intent_id,
+				});
 			}
 
 			// Format response
